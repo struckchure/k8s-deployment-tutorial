29ffd2f69a1dc73d6393d4985dc2b89b50b46b6c
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-14 17:46:13 +0100

    adds datetime field to response

--- gofiber_app/main.go
+++ gofiber_app/main.go
@@ -1,12 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{"app": "gofiber"})
+		return c.JSON(fiber.Map{
+			"app":      "gofiber",
+			"datetime": time.Now().Format("2006-01-02 3:4:5 pm"),
+		})
 	})
 
 	app.Listen(":8000")
