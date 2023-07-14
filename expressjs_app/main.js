46ee34da6d0b0c3b675adaaacba25c99ae98bf18
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-07-14 17:45:49 +0100

    adds datetime field to response

--- expressjs_app/main.js
+++ expressjs_app/main.js
@@ -4,9 +4,24 @@ const app = express();
 const port = 8000;
 
 app.get("/", (_req, res) => {
-  res.json({ app: "expressjs" });
+  const currentDate = new Date();
+
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const hours = String(currentDate.getHours()).padStart(2, "0");
+  const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+  const ampm = (hours >= 12 ? "pm" : "am").toUpperCase();
+
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${ampm}`;
+
+  res.json({
+    app: "expressjs",
+    datetime: formattedDate,
+  });
 });
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+  console.log(`INFO: app listening on port ${port}`);
 });
